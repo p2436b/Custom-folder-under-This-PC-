5d0fd508cad463abc4bcb14f7a446c3727a71a56
Peyman Bayat <p2436b@gmail.com>
2020-01-20 12:51:49 +0330

    Add code for add and delete buttons

--- ThisPC/Form1.Designer.cs
+++ ThisPC/Form1.Designer.cs
@@ -33,30 +33,26 @@
             this.rbCurrentUser = new System.Windows.Forms.RadioButton();
             this.txtFolderName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtIconFilePath = new System.Windows.Forms.TextBox();
+            this.txtFolderIconPath = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtTooltip = new System.Windows.Forms.TextBox();
+            this.txtFolderTooltip = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.rbLocalMachine);
             this.groupBox1.Controls.Add(this.rbCurrentUser);
-            this.groupBox1.Location = new System.Drawing.Point(9, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(17, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(346, 81);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(346, 75);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Install for";
@@ -64,8 +60,9 @@
             // rbLocalMachine
             // 
             this.rbLocalMachine.AutoSize = true;
-            this.rbLocalMachine.Location = new System.Drawing.Point(232, 36);
-            this.rbLocalMachine.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbLocalMachine.Checked = true;
+            this.rbLocalMachine.Location = new System.Drawing.Point(240, 40);
+            this.rbLocalMachine.Margin = new System.Windows.Forms.Padding(2);
             this.rbLocalMachine.Name = "rbLocalMachine";
             this.rbLocalMachine.Size = new System.Drawing.Size(95, 17);
             this.rbLocalMachine.TabIndex = 1;
@@ -76,160 +73,136 @@
             // rbCurrentUser
             // 
             this.rbCurrentUser.AutoSize = true;
-            this.rbCurrentUser.Location = new System.Drawing.Point(77, 36);
-            this.rbCurrentUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbCurrentUser.Location = new System.Drawing.Point(98, 40);
+            this.rbCurrentUser.Margin = new System.Windows.Forms.Padding(2);
             this.rbCurrentUser.Name = "rbCurrentUser";
-            this.rbCurrentUser.Size = new System.Drawing.Size(87, 17);
+            this.rbCurrentUser.Size = new System.Drawing.Size(84, 17);
             this.rbCurrentUser.TabIndex = 0;
-            this.rbCurrentUser.TabStop = true;
-            this.rbCurrentUser.Text = "Current  User";
+            this.rbCurrentUser.Text = "Current User";
             this.rbCurrentUser.UseVisualStyleBackColor = true;
             // 
             // txtFolderName
             // 
-            this.txtFolderName.Location = new System.Drawing.Point(86, 108);
-            this.txtFolderName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFolderName.Location = new System.Drawing.Point(107, 112);
+            this.txtFolderName.Margin = new System.Windows.Forms.Padding(2);
             this.txtFolderName.Name = "txtFolderName";
-            this.txtFolderName.Size = new System.Drawing.Size(270, 20);
+            this.txtFolderName.Size = new System.Drawing.Size(258, 20);
             this.txtFolderName.TabIndex = 1;
+            this.txtFolderName.Text = "MyFolder";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 110);
+            this.label1.Location = new System.Drawing.Point(32, 114);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Folder Name:";
             // 
-            // txtIconFilePath
+            // txtFolderIconPath
             // 
-            this.txtIconFilePath.Location = new System.Drawing.Point(86, 143);
-            this.txtIconFilePath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtIconFilePath.Name = "txtIconFilePath";
-            this.txtIconFilePath.Size = new System.Drawing.Size(270, 20);
-            this.txtIconFilePath.TabIndex = 3;
+            this.txtFolderIconPath.Location = new System.Drawing.Point(107, 182);
+            this.txtFolderIconPath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFolderIconPath.Name = "txtFolderIconPath";
+            this.txtFolderIconPath.Size = new System.Drawing.Size(257, 20);
+            this.txtFolderIconPath.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 145);
+            this.label2.Location = new System.Drawing.Point(16, 184);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Icon File Path:";
+            this.label2.Text = "Folder Icon Path:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 180);
+            this.label3.Location = new System.Drawing.Point(32, 219);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.Size = new System.Drawing.Size(70, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Tooltip:";
+            this.label3.Text = "Folder tooltip:";
             // 
-            // txtTooltip
+            // txtFolderTooltip
             // 
-            this.txtTooltip.Location = new System.Drawing.Point(86, 178);
-            this.txtTooltip.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtTooltip.Name = "txtTooltip";
-            this.txtTooltip.Size = new System.Drawing.Size(270, 20);
-            this.txtTooltip.TabIndex = 6;
+            this.txtFolderTooltip.Location = new System.Drawing.Point(107, 217);
+            this.txtFolderTooltip.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFolderTooltip.Name = "txtFolderTooltip";
+            this.txtFolderTooltip.Size = new System.Drawing.Size(257, 20);
+            this.txtFolderTooltip.TabIndex = 6;
+            this.txtFolderTooltip.Text = "My folder tooltip.";
             // 
             // btnAdd
             // 
             this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAdd.Location = new System.Drawing.Point(118, 221);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Location = new System.Drawing.Point(126, 278);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(114, 29);
             this.btnAdd.TabIndex = 7;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
             this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDelete.Location = new System.Drawing.Point(241, 221);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Location = new System.Drawing.Point(249, 278);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(114, 29);
             this.btnDelete.TabIndex = 8;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
-            // panel1
+            // txtFolderPath
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(9, 275);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(347, 81);
-            this.panel1.TabIndex = 9;
+            this.txtFolderPath.Location = new System.Drawing.Point(107, 147);
+            this.txtFolderPath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(258, 20);
+            this.txtFolderPath.TabIndex = 11;
+            this.txtFolderPath.Text = "c:\\";
             // 
-            // pictureBox1
+            // label6
             // 
-            this.pictureBox1.Image = global::ThisPC.Properties.Resources.icons8_info_24;
-            this.pictureBox1.Location = new System.Drawing.Point(19, 12);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label5
-            // 
-            this.label5.Location = new System.Drawing.Point(35, 44);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(302, 31);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "If you want later delete a folder, you should write down some where the folder GU" +
-    "ID.";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(47, 23);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Info:";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 149);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Folder Path:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 366);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(382, 323);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.txtTooltip);
+            this.Controls.Add(this.txtFolderTooltip);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtIconFilePath);
+            this.Controls.Add(this.txtFolderIconPath);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtFolderName);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Add Custom Folders under \"This PC\"";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -242,16 +215,14 @@
         private System.Windows.Forms.RadioButton rbCurrentUser;
         private System.Windows.Forms.TextBox txtFolderName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtIconFilePath;
+        private System.Windows.Forms.TextBox txtFolderIconPath;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtTooltip;
+        private System.Windows.Forms.TextBox txtFolderTooltip;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- ThisPC/Form1.cs
+++ ThisPC/Form1.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows.Forms;
 
 namespace ThisPC
@@ -15,6 +9,104 @@ namespace ThisPC
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (AddFolderToThisPC(
+                "{497bec52-4c89-4de4-aeb5-334b83bfddec}",
+                rbLocalMachine.Checked,
+                txtFolderName.Text,
+                txtFolderPath.Text,
+                txtFolderTooltip.Text,
+                txtFolderIconPath.Text))
+            {
+                MessageBox.Show("Added");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        public bool AddFolderToThisPC(string guid, bool localComputer, string folderName, string folderPath, string tooltip, string iconPath)
+        {
+            try
+            {
+                RegistryKey localKey, keyTemp, rootKey;
+
+                var hive = localComputer ? RegistryHive.LocalMachine : RegistryHive.CurrentUser;
+                localKey = Environment.Is64BitOperatingSystem
+                    ? RegistryKey.OpenBaseKey(hive, RegistryView.Registry64)
+                    : RegistryKey.OpenBaseKey(hive, RegistryView.Registry32);
+
+
+                rootKey = localKey.CreateSubKey(@"SOFTWARE\Classes\CLSID\" + guid);
+                rootKey.SetValue("", folderName, RegistryValueKind.String);
+                rootKey.SetValue("DescriptionID", unchecked(0x4), RegistryValueKind.DWord);
+                rootKey.SetValue("Infotip", tooltip, RegistryValueKind.ExpandString);
+                rootKey.SetValue("System.IsPinnedToNameSpaceTree", unchecked(0x1), RegistryValueKind.DWord);
+
+                keyTemp = rootKey.CreateSubKey("DefaultIcon");
+                keyTemp.SetValue("", iconPath, RegistryValueKind.String);
+                keyTemp.Close();
+
+                keyTemp = rootKey.CreateSubKey("InProcServer32");
+                keyTemp.SetValue("", @"%systemroot%\system32\shell32.dll", RegistryValueKind.ExpandString);
+                keyTemp.SetValue("ThreadingModel", "Both", RegistryValueKind.String);
+                keyTemp.Close();
+
+                keyTemp = rootKey.CreateSubKey("ShellFolder");
+                keyTemp.SetValue("Attributes", unchecked((int)0xf080004d), RegistryValueKind.DWord);
+                keyTemp.SetValue("FolderValueFlags", unchecked(0x29), RegistryValueKind.DWord);
+                keyTemp.SetValue("SortOrderIndex", unchecked(0x0), RegistryValueKind.DWord);
+                keyTemp.Close();
+
+                keyTemp = rootKey.CreateSubKey("Instance");
+                keyTemp.SetValue("CLSID", "{0E5AAE11-A475-4c5b-AB00-C66DE400274E}", RegistryValueKind.String);
+                keyTemp.Close();
+
+                keyTemp = rootKey.CreateSubKey(@"Instance\InitPropertyBag");
+                keyTemp.SetValue("Attributes", unchecked(0x11), RegistryValueKind.DWord);
+                keyTemp.SetValue("TargetFolderPath", folderPath, RegistryValueKind.String);
+                keyTemp.Close();
+
+                keyTemp = localKey.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\" + guid);
+                keyTemp.Close();
+
+                rootKey.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool RemoveFolderFromThisPC(string guid, bool localComputer)
+        {
+            try
+            {
+                var hive = localComputer ? RegistryHive.LocalMachine : RegistryHive.CurrentUser;
+                var localKey = Environment.Is64BitOperatingSystem
+                    ? RegistryKey.OpenBaseKey(hive, RegistryView.Registry64)
+                    : RegistryKey.OpenBaseKey(hive, RegistryView.Registry32);
+
+                localKey.DeleteSubKeyTree(@"SOFTWARE\Classes\CLSID\" + guid, false);
+                localKey.DeleteSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\" + guid, false);
+                localKey.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var guid = "{497bec52-4c89-4de4-aeb5-334b83bfddec}";
+            RemoveFolderFromThisPC(guid, rbLocalMachine.Checked);
         }
     }
 }
